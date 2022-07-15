3ac72947470627e24d32ca6600b83903a59c951c
Alex <alexjooho@gmail.com>
2022-07-15 15:32:37 -0700

    Solved averagePair and canConstructWord functions

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,28 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+// make two pointers, and move the left one if average of values is too low; move right one if average is too high;
+// be careful of edge cases (make a fail-fast)
+
+/** Determines if a pair of numbers in given array can average to the given target average
+ * accepts an array and a number, returns a boolean
+ */
+function averagePair(nums, targetAvg) {
+    if(nums.length < 2) return false;
+    let left = 0;
+    let right = nums.length - 1;
+    while(left < right) {
+        let avg = (nums[left] + nums[right]) / 2;
+        if(avg === targetAvg){
+            return true;
+        }
+        else if(avg > targetAvg) {
+            right --;
+        }
+        else {
+            left ++;
+        }
+    }
+    return false;
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,28 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+// make sure all characters in the word are in letters
+// letters can have more characters than in word
+// make frequency counter for both and make sure the value in letters >= word frequency
+
+/** checks if given word can be constructed from string of letters
+ * accepts two strings and returns boolean
+*/
+function canConstructWord(word, letters) {
+    // can add "fail fast" for if letters = 0 or is less than word length
+    if(letters.length = 0 || letters.length < word.length) return false;
+    let wordFreq = makeFreqCounter(word);
+    let lettersFreq = makeFreqCounter(letters);
+    for(let char of word) {
+        if(wordFreq[char] > lettersFreq[char]) return false;
+    }
+    return true;
+}
+
+function makeFreqCounter(string) {
+    const counter = {};
+    for(let char of string) {
+        counter[char] = counter[char] + 1 || 1;
+    }
+    return counter;
 }
